68f7314ca3d97c4641e6a9f94315ac5162065ef3
Tefilicious <144660502+Tefilicious@users.noreply.github.com>
2025-02-08 11:47:41 +0530

    created a function to split the texts

--- ML/report-generation/compare_embeddings.py
+++ ML/report-generation/compare_embeddings.py
@@ -7,3 +7,19 @@ def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.md")
     documents = loader.load()
     return documents
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
