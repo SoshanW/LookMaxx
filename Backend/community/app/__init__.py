30d9c7514ef75ff948a894e5672479e8aa686068
Vinuki <thehasa06@gmail.com>
2025-02-11 13:23:41 +0530

    figuring out database

--- Backend/community/app/__init__.py
+++ Backend/community/app/__init__.py
@@ -2,8 +2,15 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_pymongo import PyMongo
 from .config import MONGO_URI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = 'JWT_SECRET' 
+jwt = JWTManager(app)
 
 app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
--- Backend/community/app/config.py
+++ Backend/community/app/config.py
@@ -6,4 +6,4 @@ password = "rrJ6Q%.k4EqJLG9"
 encoded_username = urllib.parse.quote_plus(username)
 encoded_password = urllib.parse.quote_plus(password)
 
-MONGO_URI = f'mongodb+srv://{encoded_username}:{encoded_password}@cluster1.1nrjp.mongodb.net/mydatabase?retryWrites=true&w=majority'
+MONGO_URI = f'mongodb+srv://{encoded_username}:{encoded_password}@cluster0.mp7jz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
