80bca4d561017d71e5aad212001f54f5ef0fa7ab
mariyamj <mariyamjameela23@gmaiil.com>
2025-02-25 14:05:14 +0530

    added header

---
+++ Frontend/retailpage/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { User, LogIn } from "lucide-react";
+import '../styles/Header.css';
+
+
+
+function Header() {
+  return (
+    <header className="header">
+      {/* Logo Section */}
+      <div className="logo"></div>
+
+      {/* Navigation Menu */}
+      <nav className="nav-menu">
+        <ul className="nav-list">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#retail">Retail</a></li>
+          <li><a href="#community">Community</a></li>
+          <li><a href="#study">Study</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#ffr">FFR</a></li>
+        </ul>
+      </nav>
+
+      {/* Sign-in & User Profile Icons */}
+      <div className="auth-icons">
+        <Link to="/login">
+          <LogIn size={28} className="icon" />
+        </Link>
+        <Link to="/profile">
+          <User size={28} className="icon" />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
