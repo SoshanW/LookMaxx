b8f2487ecb60899e73fb635f83283bcfc0d198ff
mariyamj <mariyamjameela23@gmaiil.com>
2025-02-26 15:16:09 +0530

    Created hero-section

--- Frontend/retailpage/src/components/Header.jsx
+++ Frontend/retailpage/src/components/Header.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 import { User, LogIn } from "lucide-react";
 import '../styles/Header.css';
 
-
-
 function Header() {
   return (
     <header className="header">
@@ -15,7 +13,7 @@ function Header() {
       <nav className="nav-menu">
         <ul className="nav-list">
           <li><a href="#home">Home</a></li>
-          <li><a href="#retail">Retail</a></li>
+          <li><Link to="/retail">Retail</Link></li>
           <li><a href="#community">Community</a></li>
           <li><a href="#study">Study</a></li>
           <li><a href="#about">About</a></li>
--- Frontend/retailpage/src/components/retail.jsx
+++ Frontend/retailpage/src/components/retail.jsx
@@ -1,28 +1,21 @@
-import '../index.css'
+import '../app.css'
+import { motion } from "framer-motion";
 
  const Retailpage = () => {
 
   return (
     <>
       <div className="hero-content">
-          <h1 className="hero-title">
-            FIND <span className="highlight">YOUR</span> FIT
-          </h1>
-          <p className="hero-subtitle">Shop Smart, Style Confidently</p>
-          <p className="hero-tagline">
-            <span className="try-it">- Try it,</span>{' '}
-            <span className="buy-it">Buy it</span>
-          </p>
-          <button className="shop-button">
-            Shop Now
-          </button>
-        </div>
-        <div className="hero-image right">
-          <img
-            src="/api/placeholder/400/600"
-            alt="Female model in purple suit"
-          />
-        </div>
+          <div className='side-image left'>
+            <img src="left.jpg" alt="left image"/>
+          </div>
+          <div className='title'>
+            <p>Find your fit</p>
+          </div>
+          <div className='side-image right'>
+            <img src="right.jpg" alt="right image"/>
+          </div>
+      </div>
 
     </>
   )
