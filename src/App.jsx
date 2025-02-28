43b32045a1473538a9419695bd04d3c6e098f416
vonaraa <vonaraperera@gmail.com>
2025-02-28 13:36:53 +0530

    final commit for casting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CastingPage from "./CastingPage";
-import Header from "./Header";
-import Footer from "./Footer"; // Import Footer component
+import CastingPage from "./components/CastingPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"; // Import Footer component
+import './App.css';
 
 function App() {
   return (
     <>
-      <Header />
+      <Navbar />
       <CastingPage />
       <Footer />
     </>
