37800e4fd62e18fe15c41921198f47cc647f1846
mariyamj <mariyamjameela23@gmaiil.com>
2025-03-06 22:46:48 +0530

    added variables for necessary  userdata, added styles for header

--- Frontend/profilepage/src/components/profile.jsx
+++ Frontend/profilepage/src/components/profile.jsx
@@ -3,10 +3,21 @@ import { motion } from "framer-motion";
 import "../app.css";
 
 const Profile = () => {
-    // Basic placeholder component
+
+    const userData = {
+        firstName: "Mariyam",
+        lastName: "Jameela",
+        email: "mjameela@gmail.com",
+        gender: "Female",
+        username: "@mariyamj",
+        profileImage: "https://via.placeholder.com/150"
+      };
+
     return (
       <div className="profile-container">
-        <h1>Profile Page</h1>
+        <div className="profile-header-bg">
+            
+        </div>
       </div>
     );
   }
