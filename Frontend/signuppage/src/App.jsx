2abe0212b1405e51505159e782fc55edb3e9b8a6
mariyamj <mariyamjameela23@gmaiil.com>
2025-03-08 17:06:36 +0530

    added 3d models based on their respective gender

--- Frontend/signuppage/src/App.jsx
+++ Frontend/signuppage/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUpPage from "./pages/Signup";
+import SignUpPage from "./components/Signup";
 import "./styles.css";
 
 function App() {
