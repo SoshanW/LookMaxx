e96872cddfb45aa7c86f35248aca8a6fc05f076e
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-03-09 10:36:05 +0530

    Initial Commit

---
+++ Frontend/Payment Portal/payment-portal/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-purple': '#1a1327',
+        'card-bg': '#272042'
+      }
+    },
+  },
+  plugins: [],
+}
