5f3a06cb1588de3abe20a87a1b8e810f9bbf2789
Vinuki <thehasa06@gmail.com>
2025-03-10 15:14:10 +0530

    Route to email anf email funtionality handling done

--- Backend/casting/app/__init__.py
+++ Backend/casting/app/__init__.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from app.routes import main
+from app.config import Config, mail
 from app.db import init_db
 from app.config import Config
 from dotenv import load_dotenv
 from flask_cors import CORS
+from app.db import mongo
  
 load_dotenv()
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     jwt = JWTManager(app)
  
     init_db(app)
- 
-    app.register_blueprint(main)
+    mail.init_app(app)
+
     CORS(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+    
     return app
