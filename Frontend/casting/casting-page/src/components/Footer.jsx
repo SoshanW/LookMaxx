e24b555ffbab63243c2eaf23037970be6c411d4c
vonaraa <vonaraperera@gmail.com>
2025-03-10 13:44:04 +0530

    Final commit before adding the form

--- Frontend/casting/casting-page/src/components/Footer.jsx
+++ Frontend/casting/casting-page/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./styles.css";
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
@@ -11,25 +11,13 @@ const Footer = () => {
           <Link to="/contact">Contact</Link>
         </nav>
         <div className="footer-social">
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-facebook-f"></i>
           </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter"></i>
           </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-instagram"></i>
           </a>
         </div>
