fbd9428a5d77e4073fe10b04da394361541a01f9
mariyamj <mariyamjameela23@gmaiil.com>
2025-03-14 10:46:07 +0530

    created hero section

--- Frontend/retailpage/retailpage/src/components/Header.jsx
+++ Frontend/retailpage/retailpage/src/components/Header.jsx
@@ -3,6 +3,8 @@ import React from "react";
  import { User, LogIn } from "lucide-react";
  import '../styles/Header.css';
  
+ 
+ 
  function Header() {
    return (
      <header className="header">
@@ -13,7 +15,7 @@ import React from "react";
        <nav className="nav-menu">
          <ul className="nav-list">
            <li><a href="#home">Home</a></li>
-           <li><a href="#retail">Retail</a></li>
+           <li><Link to="/retail">Retail</Link></li>
            <li><a href="#community">Community</a></li>
            <li><a href="#study">Study</a></li>
            <li><a href="#about">About</a></li>
