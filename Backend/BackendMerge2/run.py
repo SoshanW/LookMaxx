4662349468cf066a0ef225201994cc41a87bbd07
Vinuki <thehasa06@gmail.com>
2025-03-21 12:53:19 +0530

    using localhost keeps giving a proxy error

--- Backend/BackendMerge2/run.py
+++ Backend/BackendMerge2/run.py
@@ -86,4 +86,4 @@ def create_unified_app():
 
 if __name__ == '__main__':
     app = create_unified_app()
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
