3458051b39cc6305a8d21d339630214e085e4459
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-03-28 12:44:24 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,22 +6,22 @@ export default defineConfig({
   server: {
     proxy: {
       '/auth': {
-        target: 'https://capp-lookmaxx.happywave-d54ff16c.southeastasia.azurecontainerapps.io' || 'http://127.0.0.1:5000',
+        target: process.env.VITE_API_URL || 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false
       },
       '/ffr': {
-        target: 'https://capp-lookmaxx.happywave-d54ff16c.southeastasia.azurecontainerapps.io' || 'http://127.0.0.1:5000',
+        target: process.env.VITE_API_URL || 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false
       },
       '/casting': {
-        target: 'https://capp-lookmaxx.happywave-d54ff16c.southeastasia.azurecontainerapps.io' || 'http://127.0.0.1:5000',
+        target: process.env.VITE_API_URL || 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       },
       '/payments': {
-        target: 'https://capp-lookmaxx.happywave-d54ff16c.southeastasia.azurecontainerapps.io' || 'http://127.0.0.1:5000',
+        target: process.env.VITE_API_URL || 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false
       }
